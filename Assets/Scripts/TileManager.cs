d2bf17ca6cf590edaaea3d46228c89ece1520dce
KvartzCode <113012348+KvartzCode@users.noreply.github.com>
2024-01-27 19:06:23 +0100

    Tiling

--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -4,5 +4,35 @@ using UnityEngine;
 
 public class TileManager : MonoBehaviour
 {
-   
+    public enum TileDirection
+	{
+        Up,
+        Down,
+        Left,
+        Right
+	}
+
+	[SerializeField] float moveDistanceHorizontal = 100;
+	[SerializeField] float moveDistanceVertical = 100;
+
+    public void MoveTile(TileDirection direction)
+	{
+		switch (direction)
+		{
+			case TileDirection.Up:
+				transform.Translate(0, moveDistanceVertical, 0);
+				break;
+			case TileDirection.Down:
+				transform.Translate(0, -moveDistanceVertical, 0);
+				break;
+			case TileDirection.Left:
+				transform.Translate(-moveDistanceHorizontal, 0, 0);
+				break;
+			case TileDirection.Right:
+				transform.Translate(moveDistanceHorizontal, 0, 0);
+				break;
+			default:
+				break;
+		}
+	}
 }
