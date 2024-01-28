e29b7f1e0d04b885bfe2a6d0cf3238d3cd56ac9f
MikaelahJ <mikaelah.jenkins@outlook.com>
2024-01-28 15:54:55 +0100

    stuff

--- Assets/Scripts/Audio/VisualSoundCues.cs
+++ Assets/Scripts/Audio/VisualSoundCues.cs
@@ -1,7 +1,9 @@
+using JetBrains.Annotations;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class VisualSoundCues : MonoBehaviour
 {
@@ -9,30 +11,45 @@ public class VisualSoundCues : MonoBehaviour
 
     [SerializeField] GameObject visualSound;
     [SerializeField] RectTransform canvasRect;
+    [SerializeField] RectTransform tentArrow;
 
     [SerializeField] float visualRadius = 14.8f;
 
     private void Start()
     {
         Instance = this;
-      // visualRadius = GetComponent<CircleCollider2D>().radius;
+    }
+
+    public void SetArrowPos(Vector3 tentPosition)
+    {
+        if (Vector2.Distance(tentPosition, transform.position) > visualRadius)
+        {
+            tentArrow.gameObject.SetActive(true);
+
+            Vector2 dir = (transform.position - tentPosition).normalized;
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.up);
+
+            rotation.eulerAngles = new(0, 0, angle + 90);
+            tentArrow.anchoredPosition = GetCanvasPosition(tentPosition);
+            tentArrow.rotation = rotation;
+        }
+        else
+            tentArrow.gameObject.SetActive(false);
     }
 
     public void MadeSound(Vector3 origin)
     {
-        if (Vector2.Distance(origin, transform.position) > visualRadius)
-        {
-            Vector2 dir = (transform.position - origin).normalized;
-            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-            Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.up);
+        Vector2 dir = (transform.position - origin).normalized;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.up);
 
-            rotation.eulerAngles = new(0, 0, angle);
-            RectTransform image = Instantiate(visualSound, canvasRect).GetComponent<RectTransform>();
-            image.anchoredPosition = GetCanvasPosition(origin);
-            image.rotation = rotation;
+        rotation.eulerAngles = new(0, 0, angle - 90);
+        RectTransform image = Instantiate(visualSound, canvasRect).GetComponent<RectTransform>();
+        image.anchoredPosition = GetCanvasPosition(origin);
+        image.rotation = rotation;
 
-            Destroy(image.gameObject, 2f);
-        }
+        Destroy(image.gameObject, 3f);
     }
 
     private Vector2 GetCanvasPosition(Vector2 _origin)
